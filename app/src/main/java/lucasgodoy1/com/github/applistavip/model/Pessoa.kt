77f6f0a1478d16527d694598e3aed70908a59117
Lucas Godoy <lucassdegodoy@hotmail.com>
2024-11-02 17:35:03 -0300

    clean code

--- app/src/main/java/lucasgodoy1/com/github/applistavip/model/Pessoa.kt
+++ app/src/main/java/lucasgodoy1/com/github/applistavip/model/Pessoa.kt
@@ -1,12 +1,11 @@
 package lucasgodoy1.com.github.applistavip.model
 
 
-class Pessoa (nome : String, segundoNome: String, cursoDeImteresse: String, telefoneContato : String){
-     val primeiroNome: String = nome
-     val segundoNome: String = segundoNome
-     var cursoDeImteresse: String = cursoDeImteresse
-     var telefoneContato: String = telefoneContato
-
+class Pessoa(nome: String, segundoNome: String, cursoDeImteresse: String, telefoneContato: String) {
+    val primeiroNome: String = nome
+    val segundoNome: String = segundoNome
+    var cursoDeImteresse: String = cursoDeImteresse
+    var telefoneContato: String = telefoneContato
 
 
     override fun toString(): String {
